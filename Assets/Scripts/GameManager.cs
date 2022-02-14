506c9844f0d4c6b239c1c3e224fad34969b4dc55
LeBuuToan <98265816+LeBuuToan@users.noreply.github.com>
2022-02-14 20:58:30 +0700

    Upload Assets Project Block Game 2D

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+
+	public GameObject pauseMenuUI;
+
+	public bool paused = false;
+
+    void Awake()
+    {
+        instance = GetComponent<GameManager>();
+    }
+
+    public void ResumeButton()
+    {
+        paused = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PauseButton()
+    {
+        paused = true;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+
+    }
+
+    public void ReplayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGameButton()
+    {
+        Application.Quit();
+    }
+}
